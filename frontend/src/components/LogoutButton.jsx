8e7cbab0d6a1efb53987bb84664fc5f7822d9a7c
Nic <nicholasstm@gmail.com>
2025-06-11 16:37:54 +0100

    gave button component new optional fields for more flexibility

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -1,49 +1,26 @@
-// To be fixed
+import Button from "./Button";
 import { LogOutIcon } from "lucide-react";
 
-function LogoutButton({ className }) {
-  function logOut() {
-    localStorage.clear();
-    sessionStorage.clear();
-
-    localStorage.removeItem("token");
-    window.dispatchEvent(new Event('authChange'));
-    
-    window.location.replace("/");
-  }
-
-  return (
-  <button 
-    onClick={logOut} 
-    className={`dropdown-item logout-item ${className}`}
-  >
-    <LogOutIcon className="dropdown-icon" />
-    Log out
-  </button>
-  )
-}
-export default LogoutButton;
-
-import { useNavigate } from "react-router-dom";
-import Button from "./Button";
-
-
-const LogoutButton = () => {
-    const navigate = useNavigate();
-
-// function LogoutButton() {
-//   function logOut() {
-//     localStorage.removeItem("token");
-//     window.dispatchEvent(new Event('authChange'))
-//     window.location.href = "/"
-//  }
+const LogoutButton = ({ className }) => {
 
     const handleLogout = () => {
-        localStorage.removeItem("token");
-        navigate("/");
+      localStorage.clear();
+      sessionStorage.clear();
+
+      localStorage.removeItem("token");
+      window.dispatchEvent(new Event('authChange'));
+      
+      window.location.replace("/");
     };
 
-    return <Button onClick={handleLogout}>Logout</Button>;
+    return (
+    <Button 
+      onClick={handleLogout}
+      buttonIcon={<LogOutIcon className="dropdown-icon" />}
+      buttonText="Log out"
+      optionalStyling={`dropdown-item logout-item ${className}`}
+    />
+  )
 };
 
 export default LogoutButton;
